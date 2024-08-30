2413eee78204e4dcc0781580fad31ce8e632b016
souzafcharles <souzach@outlook.com>
2024-08-29 22:32:12 -0300

    Java version

---
+++ 01_introduction_to_algorithms/Java/src/com/souza/charles/BinarySearch.java
@@ -0,0 +1,38 @@
+package com.souza.charles;
+/**
+Book: Grokking Algorithms - An Illustrated Guide for Programmers and Other Curious People
+Author: Aditya Bhargava
+Publisher: Manning Publications Co.
+Example adapted by: Charles Fernandes de Souza
+Date: August 29, 2024
+ */
+
+public class BinarySearch {
+    
+    public static Integer binarySearch(int[] list, int item) {
+        int lower = 0;
+        int upper = list.length - 1;
+
+        while (lower <= upper) {
+            int middle = (lower + upper) / 2;  // Calculate the middle index
+            int guess = list[middle];  // Guess the middle element
+
+            if (guess == item) {  // If the guess is correct, return the index
+                return middle;
+            }
+            if (guess > item) {  // If the guess is too high, adjust the upper bound
+                upper = middle - 1;
+            } else {  // If the guess is too low, adjust the lower bound
+                lower = middle + 1;
+            }
+        }
+        return null;  // Return null if the item is not found
+    }
+
+    public static void main(String[] args) {
+        int[] myList = {1, 3, 5, 7, 9};
+
+        System.out.println(binarySearch(myList, 3));  // => 1
+        System.out.println(binarySearch(myList, -1)); // => null
+    }
+}
