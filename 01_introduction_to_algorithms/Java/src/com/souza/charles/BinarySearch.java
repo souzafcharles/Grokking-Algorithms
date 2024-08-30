64ebbedd9a9b4984be9d8522385163f191ca1db2
Charles Fernandes de Souza <souzach@outlook.com>
2024-08-29 22:46:36 -0300

    Update Binary Search - Java Version

--- 01_introduction_to_algorithms/Java/src/com/souza/charles/BinarySearch.java
+++ 01_introduction_to_algorithms/Java/src/com/souza/charles/BinarySearch.java
@@ -8,31 +8,38 @@ Date: August 29, 2024
  */
 
 public class BinarySearch {
-    
+
+    /**
+     * Conducts a binary search on a sorted integer array to locate the index of a specified item.
+     *
+     * @param list A sorted array of integers.
+     * @param item The item to be searched for.
+     * @return The index of the item within the array, or null if the item is not found.
+     */
     public static Integer binarySearch(int[] list, int item) {
         int lower = 0;
         int upper = list.length - 1;
 
         while (lower <= upper) {
-            int middle = (lower + upper) / 2;  // Calculate the middle index
-            int guess = list[middle];  // Guess the middle element
+            int middle = (lower + upper) / 2; // Calculate the middle index
+            int guess = list[middle]; // Guess the middle element
 
-            if (guess == item) {  // If the guess is correct, return the index
+            if (guess == item) { // If the guess is correct, return the index
                 return middle;
-            }
-            if (guess > item) {  // If the guess is too high, adjust the upper bound
+            } else if (guess > item) { // If the guess is too high, adjust the upper bound
                 upper = middle - 1;
-            } else {  // If the guess is too low, adjust the lower bound
+            } else { // If the guess is too low, adjust the lower bound
                 lower = middle + 1;
             }
         }
-        return null;  // Return null if the item is not found
+
+        return null; // Return null if the item is not found
     }
 
     public static void main(String[] args) {
         int[] myList = {1, 3, 5, 7, 9};
 
-        System.out.println(binarySearch(myList, 3));  // => 1
+        System.out.println(binarySearch(myList, 3)); // => 1
         System.out.println(binarySearch(myList, -1)); // => null
     }
 }
